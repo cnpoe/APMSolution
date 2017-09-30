b5c4437a8b954b1c3d923e6e2fb882e8ac802702
cnpoe <cnpoek@gmail.com>
2017-09-29 23:42:06 -0500

    se crea la navegacion para list de products y home

--- app/app.js
+++ app/app.js
@@ -9,12 +9,21 @@
     app.config(
         ["$stateProvider", "$urlRouterProvider",
         function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise("/products");
+            $urlRouterProvider.otherwise("/");
             $stateProvider
+                .state("home", {
+                    url: "/",
+                    templateUrl: "app/welcomeView.html"
+                })
                 .state("productList",{
                     url: "/products",
                     templateUrl:"app/products/productListView.html",
                     controller:"ProductListCtrl as vm"
+                })
+                .state("productEdit", {
+                    url: "/products/edit/:productId",
+                    templateUrl: "app/products/productEditView.html",
+                    controller: "ProductEditCtrl as vm"
                 })
         }]
     );
