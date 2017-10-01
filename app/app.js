e5b935ec5194c36941fce23f616068fe3999f7ae
cnpoe <cnpoek@gmail.com>
2017-10-01 00:34:42 -0500

    detalle corregido para todos los productos

--- app/app.js
+++ app/app.js
@@ -28,7 +28,17 @@
                 .state("productDetail",{
                     url: "/products/:productId",
                     templateUrl: "app/products/productDetailView.html",
-                    controller: "ProductDetailCtrl as vm"
+                    controller: "ProductDetailCtrl as vm",
+                    resolve: {
+                        productResource: "productResource",
+
+                        product: function (productResource, $stateParams) {
+                            var productId = $stateParams.productId;
+                            return productResource.get(
+                                {productId: productId}).$promise;
+                        }
+                    }
+
                 })
         }]
     );
